c8cfd71af7d0222b4b5f70110db206257bd06d9d
Joel Hoyt <joel.hoyt@unb.ca>
2019-11-30 12:17:22 -0400

    working on 2d bool array

--- src/main/java/ca/unbsj/cs2383/Maze.java
+++ src/main/java/ca/unbsj/cs2383/Maze.java
@@ -10,30 +10,36 @@ import java.util.Set;
 public class Maze {
     static final int LEFT=0, RIGHT=1, UP=2, DOWN=3;
 
-    static final int CELLSIZE=25;
+    static final int CELLSIZE=20;
     static final int twoDBooleanSet=1;
     static final int bitSetSet=2;
     static final int hashSetSet=3;
     static final int bloomFilterSet=4;
-    
+
 
     static final int N = 40; // grid is NxN
     static final double GRAPHICSSCALE = 1.0/N;
-    
+
     int topLeft, bottomRight; // node IDs
     Random r;
     UF uf;  // union find structures
     Set<String> removedWalls;
 
+    // 2d array of walls, N x 2N
+    // bottom wall of cell[i][j] corresponds to walls[i][2j]
+    // right wall  of cell[i][j] corresponds to walls[i][2j+1]
+    boolean[][] walls;
+
+
     boolean drawOnGraphicsScreen;
-    
+
     public static void main(String [] args) {
         int setKindCode = Integer.parseInt(args[0]);
         if (setKindCode < 0 || setKindCode > bloomFilterSet)
             throw new RuntimeException("illegal setKind Code on cmd line");
-        
+
         Maze m = new Maze(setKindCode,true); // will draw on grahics
-        
+
         m.generate();
         m.printAsciiGraphics();
      }
@@ -54,14 +60,19 @@ public class Maze {
 
     public Maze(int setKindCode, boolean graphicsDraw) {
         uf = new UF(N*N);
+        walls = new boolean[N][2*N];
+        for (boolean[] a : walls)
+          for (boolean b : a)
+            b = true;
+
         // our maze requires us to get from top left
         // to bottom right.
 
-        
+
         topLeft = xyToId(0,0);
         bottomRight = xyToId(N-1,N-1);
         r = new Random();
- 
+
         drawOnGraphicsScreen = graphicsDraw;
         if (graphicsDraw) {
             StdDraw.setCanvasSize(N*CELLSIZE,N*CELLSIZE);
@@ -74,7 +85,7 @@ public class Maze {
             // things are basically "upside down" when visualized
             // with it.....unless we do these crazy (1-y) transform to
             // fix this.
-            
+
             for (int i=0; i <= N; ++i) {
                 StdDraw.line(0, 1- i/(double)N, 1, 1-i/(double) N);
                 StdDraw.line(i/(double)N, 0, i/(double)N,  1);
@@ -82,7 +93,7 @@ public class Maze {
             StdDraw.setPenRadius(0.006); // don't want shadow of former line
         }
     }
-    
+
     void generate() {
         while (!uf.connected(topLeft, bottomRight)) {
             // choose a random cell
@@ -90,13 +101,13 @@ public class Maze {
             int randY = r.nextInt(N);
             int randId = xyToId(randX,randY);
 
-            // choose one of its 4 neighbours 
+            // choose one of its 4 neighbours
             int randDir = r.nextInt(4);
-            
+
             // knock down a wall, if present, between cell and
             // its chosen neighbour if they are not yet in the
             // same component
-            
+
             switch(randDir) {
             case LEFT:
                 if (randX != 0)
@@ -117,31 +128,41 @@ public class Maze {
             }
         }
     }
-    
-    
+
+
     void connectIfNotConnected(int id1, int id2) {
-        if ( ! uf.connected(id1, id2)) {
+        if ( ! uf.connected(id1, id2))
+        {
             // knock out the wall and thereby merge components
             uf.union(id1,id2);
-            
-            if (drawOnGraphicsScreen) {
-                // erase the wall
-                StdDraw.setPenColor(StdDraw.WHITE);
-                int x1 = idToX(id1); int y1 = idToY(id1);
-                int x2 = idToX(id2); int y2 = idToY(id2);
-                if (x1 == x2) {
-                    // vertical adjacency needs a horizontal line seg
-                    int greaterY=Math.max(y1,y2);
-                    StdDraw.line(x1*GRAPHICSSCALE+0.002, 1-greaterY*GRAPHICSSCALE,
-                                 (x1+1)*GRAPHICSSCALE-0.002, 1-greaterY*GRAPHICSSCALE);
-                } else {  // need a vertical line set
-                    int greaterX=Math.max(x1,x2);
-                    StdDraw.line(greaterX*GRAPHICSSCALE, 1-(y1*GRAPHICSSCALE+0.002),
-                                 greaterX*GRAPHICSSCALE, 1-((y1+1)*GRAPHICSSCALE-0.002));
 
-                }
+            int x1 = idToX(id1); int y1 = idToY(id1);
+            int x2 = idToX(id2); int y2 = idToY(id2);
+
+
+
+
+            if (drawOnGraphicsScreen)
+            {
+              // erase the wall
+              StdDraw.setPenColor(StdDraw.WHITE);
+
+              if (x1 == x2)
+              {
+                  // vertical adjacency needs a horizontal line seg
+                  int greaterY=Math.max(y1,y2);
+                  StdDraw.line(x1*GRAPHICSSCALE+0.002, 1-greaterY*GRAPHICSSCALE,
+                               (x1+1)*GRAPHICSSCALE-0.002, 1-greaterY*GRAPHICSSCALE);
+              }
+              else
+              {
+                // need a vertical line set
+                int greaterX=Math.max(x1,x2);
+                StdDraw.line(greaterX*GRAPHICSSCALE, 1-(y1*GRAPHICSSCALE+0.002),
+                             greaterX*GRAPHICSSCALE, 1-((y1+1)*GRAPHICSSCALE-0.002));
+              }
             }
-        }
+          }
         else {
             // nothing.  Even if there is a wall here, it does
             // nothing except make the maze harder.  So leave
@@ -149,11 +170,11 @@ public class Maze {
         }
     }
 
-    // for debugging, 
+    // for debugging,
     // show members of the connected component of (0,0)
     // with blanks, * for non-members
     // Note: this is not a visualization of the maze
-    
+
     void print(int howMuch) {
         if (howMuch > N) howMuch = N;
         for (int y=0; y < howMuch; ++y) {
@@ -167,8 +188,9 @@ public class Maze {
     }
 
 
-    void printAsciiGraphics() {
+    void printAsciiGraphics()
+    {
         System.out.println("printAsciiGraphics: write me");
-    }        
+    }
 
 }
